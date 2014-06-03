eddc8c1b235b606ac03aed26b0084f7c855193fa
Postmodern <postmodern.mod3@gmail.com>
2014-06-03 12:41:51 -0700

    Added an test Thor task.

---
+++ ruby/thor/test.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'thor'
+
+class Test < Thor
+
+  # option :opt, desc: 'an option'
+  # option :flag, type: :boolean,
+  #               desc: 'a boolean flag'
+
+  desc 'test', 'test task'
+  # method_option :opt, desc: 'an option'
+  # method_option :flag, type: :boolean,
+  #                      desc: 'a boolean flag'
+  def test
+    puts 'test'
+  end
+
+end
+
+if $0 == __FILE__
+  Test.start(ARGV)
+end

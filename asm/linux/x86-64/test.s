bbcaac76694324b02c69e61ba89cdacda193eab8
Postmodern <postmodern.mod3@gmail.com>
2017-03-01 20:09:45 -0800

    ASM programs now print "test".

--- asm/linux/x86-64/test.s
+++ asm/linux/x86-64/test.s
@@ -1,9 +1,18 @@
 .globl	_start
-
 .text
 _start:
-	movq	$0x41, %rax
+	movq	$length, %rdx
+	movq	$string, %rsi
+	movq	$0x1, %rdi # stdout
+	movq	$0x1, %rax # sys_write
+	syscall
+
 _exit:
 	movq	$0x0, %rdi
 	movq	$0x3c, %rax # sys_exit
 	syscall
+
+.data
+string:
+	.ascii "test\n"
+	length = . - string
--- asm/linux/x86/test.s
+++ asm/linux/x86/test.s
@@ -1,9 +1,18 @@
 .globl	_start
-
 .text
 _start:
-	movl	$0x41, %eax
+	movl	$length, %edx
+	movl	$string, %ecx
+	movl	$0x1, %ebx # stdout
+	movl	$0x4, %eax # sys_write
+	int	$0x80
+
 _exit:
 	movl	$0x0, %ebx
 	movl	$0x1, %eax # sys_exit
 	int	$0x80
+
+.data
+string:
+	.ascii	"test\n"
+	length = . - string

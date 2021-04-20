4f6bd5a2df97aa0a7e2955e2555a576ee0987823
Postmodern <postmodern.mod3@gmail.com>
2021-04-19 19:43:31 -0700

    Added an example Crystal shard with a binary target.

---
+++ crystal/shard/spec/test_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Test do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ crystal/shard/src/test.cr
@@ -0,0 +1,9 @@
+module Test
+  VERSION = "0.1.0"
+
+  def self.test
+    puts "test"
+  end
+end
+
+Test.test
